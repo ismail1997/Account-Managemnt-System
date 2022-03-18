aab86aa54f4e9e1179ac226f21c98ef504339593
unknown <bouaddi1997@gmail.com>
2022-03-18 20:22:44 +0100

    change some configuration

--- src/entities/User.java
+++ src/entities/User.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data @ToString @AllArgsConstructor @NoArgsConstructor
+@Data @AllArgsConstructor @ToString @NoArgsConstructor
 public class User {
 	
 	private int id;
--- src/sessionManagement/SessionManagement.java
+++ src/sessionManagement/SessionManagement.java
@@ -1,14 +1,12 @@
 package sessionManagement;
 
-import java.util.Date;
+
 
 import entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import utils.Tools;
-import utils.UserTools;
 
 @Data @ToString @AllArgsConstructor @NoArgsConstructor
 public class SessionManagement {
@@ -16,12 +14,7 @@ public class SessionManagement {
 	private User sessionUser;
 	private String sessionDate;
 
-	public static void main(String[] args) throws Exception {
-		User user = UserTools.getOneUser(1);
-		SessionManagement s  = new SessionManagement(1,user,new Date()+"");
-		Tools.writeObjectAsStringToFileWithoutAppend(s, SessionManagementTools.filePath);
-		System.out.println(s);
-	}
+
 
 }
 
